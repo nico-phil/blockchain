ce3821168081a08ddf3dc05aed84d8cf9746699a
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2025-05-17 20:22:21 +0300

    add wallet server

--- blockchain_server/main.go
+++ blockchain_server/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"log"
 	"os"
 	"strconv"
 )
+
+func init(){
+
+}
 
 func main(){
 	port, _ := strconv.Atoi(os.Getenv("port"))
 
 	app := NewBlockchainServer(port)
 	
-	app.Run()
+	err := app.Run()
+	if err != nil{
+		log.Fatal("error starting server", err)
+	}
 	
 }
