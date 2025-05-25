9a5db33b74dff0fd8313f13ca50bf5c6e431669b
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2025-05-25 23:55:45 +0300

    add find neighbors func

--- utils/neighbor.go
+++ utils/neighbor.go
@@ -2,13 +2,33 @@ package utils
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"regexp"
+	"time"
 )
+
+var PATTERN = regexp.MustCompile()
 
 func IsFoundHost(host string, port uint16) bool {
 	target := fmt.Sprintf("%s:%d", host, port)
 	fmt.Println(target)
 
-	// _, err := net.DialTimeout()
+	_, err := net.DialTimeout("tcp", target, 1 * time.Second)
+	if err != nil {
+		log.Printf("%s %v\n", target, err)
+		return false
+	}
 
-	return false
+	return true
+}
+
+func FindNeigbors(myHost string, myPort, startIp, endIp, startPort, endPort int) []string {
+	address := fmt.Sprintf("%s:%d", myHost, myPort)
+	m := PATTERN.FindStringSubmatch(myHost)
+	if m != nil {
+		return nil
+	}
+
+	return []string{}
 }
